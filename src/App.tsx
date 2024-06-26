5036af2977c8d24b3082091ef8f75dfcdc7bf97c
Weslley <weslleyfellipe1@gmail.com>
2024-06-26 08:17:50 -0300

    Introdução front end

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+);
+}
+export default App;
